33bb17b3a2b8232b5e14fb87a67592b895e14a67
Iliyan-H-Iliev <iliyan.hr.iliev@gmail.com>
2023-08-24 20:30:50 +0100

    Upload Files

--- system/forms.py
+++ system/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
-
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 
@@ -31,3 +32,24 @@ class LoginForm(FlaskForm):
     password = PasswordField("Парола", validators=[DataRequired(), Length(min=6)])
     remember = BooleanField("Запомни ме")
     submit = SubmitField("Вход")
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField("Потребителско име", validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField("Емейл", validators=[DataRequired(), Email(), Length(min=6, max=50)])
+    picture = FileField("Обнови профилна снимка", validators=[FileAllowed(["jpg", "png"])])
+    submit = SubmitField("Обнови")
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+
+            if user:
+                raise ValidationError("Това потребителско име е заето. Моля изберете друго потребителско име.")
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+
+            if user:
+                raise ValidationError("Този емейл е зает. Моля изберете друг емейл.")
--- system/routes.py
+++ system/routes.py
@@ -1,6 +1,9 @@
+import os
+import secrets
+from PIL import Image
 from system import app, db, bcrypt
 from flask import render_template, url_for, flash, redirect, request
-from system.forms import RegistrationForm, LoginForm
+from system.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from system.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -85,8 +88,47 @@ def logout():
     return redirect(url_for("home"))
 
 
-@app.route("/account")
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    file_name, file_extension = os.path.splitext(form_picture.filename)
+    picture_filename = random_hex + file_extension
+    picture_path = os.path.join(app.root_path, "static/profile_pics", picture_filename)
+
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
+    return picture_filename
+
+
+def save_recipe_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    file_name, file_extension = os.path.splitext(form_picture.filename)
+    picture_filename = random_hex + file_extension
+    picture_path = os.path.join(app.root_path, "static/recipe_pics", picture_filename)
+    form_picture.save(picture_path)
+    return picture_filename
+
+
+@app.route("/account", methods=["GET", "POST"])
 @login_required
 def account():
     image_file = url_for("static", filename="profile_pics/" + current_user.image_file)
-    return render_template("account.html", title="Account", image_file=image_file)
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_filename = save_picture(form.picture.data)
+            current_user.image_file = picture_filename
+
+        current_user.username = form.username.data
+        current_user.emai = form.email.data
+        db.session.commit()
+        flash("Вашият профил беше обновен", "success")
+        return redirect(url_for("account"))
+    elif request.method == "GET":
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+
+    return render_template("account.html", title="Account", image_file=image_file, form=form)
+
